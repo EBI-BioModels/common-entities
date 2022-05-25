f085ac30a8043215413e2f16f942334131b8e767
Tung Nguyen <nvntung@gmail.com>
2022-05-25 10:38:44 +0100

    Refactor the Role entity

---
+++ src/main/java/net/biomodels/jummp/entities/security/Role.java
@@ -0,0 +1,31 @@
+package net.biomodels.jummp.entities.security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Table(name = "role")
+@Entity
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "authority")
+    private String authority;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
